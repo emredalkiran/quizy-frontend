edb2999d298f5b5ef4af4ff9b61d20a5dc5ccb8a
emredalkiran <emre59dalkiran@gmail.com>
2020-11-11 10:16:24 +0300

    Form validation on submit

--- src/components/login-form.js
+++ src/components/login-form.js
@@ -10,8 +10,8 @@ const validateSingleField = (fieldName, value) => {
   try {
      return [false, Yup.reach(loginValidationSchema, fieldName).validateSync(value)]
    }
-   catch(e) {
-    return [true, e]
+   catch(err) {
+    return [true, err]
    }
  }
 
@@ -41,23 +41,43 @@ const validateSingleField = (fieldName, value) => {
       setPasswordErrorMessage(passwordStatus[1].message)
     }
  }, [password])
+
+ const handleSubmit = async (e)=> {
+  setTouched({email:true, password: true}) 
+  e.preventDefault()
+  try {
+    const loginData = loginValidationSchema.validateSync({email: email, password: password}, { abortEarly: false })
+    console.log(loginData)
+  }
+  catch (err) {
+    let errorFields = []
+    err.inner.forEach(error => {
+      if (!errorFields.includes(error.path)) {
+        errorFields.push(error.path)
+        error.path === 'email' ? setEmailErrorMessage(error.message) : setPasswordErrorMessage(error.message)
+
+      }
+    })
+  }
+ }
   return (
     <form>
       <div className="login-input-elements-wrapper">
       <div>
         <label className="label" htmlFor="email">Email</label>
-        <input type="email" name="email" placeholder="Email address" className={`input ${(emailErrorMessage !== '' && touched.email) ? 'is-danger': ''}`} onFocus={()=>setTouched({...touched, email:true})} onChange={ (e)=>setEmail(e.target.value)} value={email}/>
+        <input type="email" name="email" placeholder="Email address" className={`input ${(emailErrorMessage !== '' && touched.email) ? 'is-danger': ''}`} onFocus={()=>setTouched({...touched, email:true})} onChange={ e => setEmail(e.target.value)} value={email}/>
         { (emailErrorMessage !== '' && touched.email) ? (
          <div className="input-error">{emailErrorMessage}</div> ) : ''
       }
       </div>
       <div>
         <label className="label" htmlFor="password">Password</label>
-        <input type="password" name="password" className={`input ${(passwordErrorMessage !== '' && touched.password) ? 'is-danger': ''}`}  placeholder="Password" onFocus={()=>setTouched({...touched, password:true})} onChange={ (e)=> { setPassword(e.target.value)}} value={password}/>
+        <input type="password" name="password" className={`input ${(passwordErrorMessage !== '' && touched.password) ? 'is-danger': ''}`}  placeholder="Password" onFocus={()=>setTouched({...touched, password:true})} onChange={ e => { setPassword(e.target.value)}} value={password}/>
         { (passwordErrorMessage !== '' && touched.password) ? (
          <div className="input-error">{passwordErrorMessage}</div>
        ) : null}
       </div>
+      <button type="submit" className="button is-primary is-fullwidth has-text-weight-medium" onClick={e=>handleSubmit(e)}>Continue</button>
       </div>
     </form> 
   )
