4e3c848db9f64c4ff7cb4c9943401f6d4ac0a353
emredalkiran <emre59dalkiran@gmail.com>
2020-12-02 15:03:58 +0300

    Handle unauthorized authentication responses

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -4,19 +4,32 @@ import axios from 'axios'
 
 
 export const login = createAsyncThunk('auth/login', async (credentials) => {
-  const response = await axios.post(`${process.env.REACT_APP_API_SERVER}/api/v1/user/login`, credentials)
-  return response
+
+  try {
+      const response = await axios.post(`${process.env.REACT_APP_API_SERVER}/api/v1/user/login`, credentials)
+      return response.data
+  } catch (err){
+    if (err.response.status === 401)
+      return err.response.data.response
+    else{
+      return {success:false, error: "Oops, something went wrong!"}
+    }
+  }
+  
 })
 
 export const signup = createAsyncThunk('auth/signup', async (credentials) => {
-  console.log("Inside signup ",process.env.REACT_APP_API_SERVER)
   try {
     const response = await axios.post(`${process.env.REACT_APP_API_SERVER}/api/v1/user/signup`, credentials)
     return response.data
   } catch (err) {
     //TODO: handle async network request error
-    console.log(err)
-    return 'Error'
+
+      if (err.response.status === 400)
+        return err.response.data.response
+      else{
+        return {success:false, error: "Please check your credentials"}
+      }
   } 
 })
 
@@ -24,10 +37,7 @@ const authSlice = createSlice({
   name: 'auth',
   initialState: {
     isLoggedIn: false,
-    user: {
-      name: '',
-      lastName: ''
-    }
+    name: ''
   },
   reducers: {
     loggedIn(sliceState, action) {
@@ -36,7 +46,8 @@ const authSlice = createSlice({
   },
   extraReducers: {
     [login.fulfilled]: (sliceState, action) => {
-
+      sliceState.isLoggedIn = true
+      sliceState.name = action.payload.name
     },
     [signup.pending]: (sliceState, action) => {
       sliceState.authError = ''
@@ -45,8 +56,10 @@ const authSlice = createSlice({
     [signup.fulfilled]: (sliceState, action) => {
       if (!action.payload.success) {
         sliceState.isLoggedIn = false
+        sliceState.name = ''
       } else {
         sliceState.isLoggedIn = true
+        sliceState.name = action.payload.name
       }
     },
     [signup.rejected]: (sliceState, action)=> {
@@ -56,8 +69,8 @@ const authSlice = createSlice({
   }
 })
 
-export const selectUser = state => state.auth.user
-export const isLoggedIn = state => state.auth.isLoggedIn
+export const selectUserName = state => state.auth.name
+export const selectLoginStatus = state => state.auth.isLoggedIn
 
 const reducer = authSlice.reducer
 
