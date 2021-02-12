3f9706316d6ab2aafae779bfbd0d78438689a097
emredalkiran <emre59dalkiran@gmail.com>
2021-02-12 11:34:07 +0300

    Use single quote

--- src/components/Pages/Home/index.js
+++ src/components/Pages/Home/index.js
@@ -1,27 +1,29 @@
-
-
-
 function Home() {
-  return(
-    <section className="hero is-large">
-      <div className="hero-body">
-        <div className="is-flex is-justify-content-space-between">
-          <div className="is-flex is-align-items-center">
+  return (
+    <section className='hero is-large'>
+      <div className='hero-body'>
+        <div className='is-flex is-justify-content-space-between'>
+          <div className='is-flex is-align-items-center'>
             <div>
-              <h1 className="title">
-                Build forms, surveys <br />or quizzes easily
+              <h1 className='title'>
+                Build forms, surveys <br />
+                or quizzes easily
                 <br />
               </h1>
-              <h2 className="subtitle">
+              <h2 className='subtitle'>
                 Make your forms fun to complete. Get valuable insights.
               </h2>
-              <button className="button is-hero-button">
+              <button className='button is-hero-button'>
                 Get started for free
               </button>
-             </div>
+            </div>
           </div>
-          <div >
-            <img src="/assets/img/hero-survey-image.png" className="hero-image" alt="survey"/>
+          <div>
+            <img
+              src='/assets/img/hero-survey-image.png'
+              className='hero-image'
+              alt='survey'
+            />
           </div>
         </div>
       </div>
