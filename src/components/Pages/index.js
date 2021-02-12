fb03acb0c0fb385bf57a4fab86dd69b9337ad5a9
emredalkiran <emre59dalkiran@gmail.com>
2021-02-12 10:22:27 +0300

    Remove unused icons

---
+++ src/components/Pages/index.js
@@ -0,0 +1,8 @@
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Solutions from './Solutions'
+import Products from './Products'
+import Pricing from './Pricing'
+
+export { Home, About, Contact, Solutions, Products, Pricing }
