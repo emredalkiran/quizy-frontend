8bf734022362ec04ee1715c021c0645406814a8e
emredalkiran <emre59dalkiran@gmail.com>
2021-02-15 16:24:18 +0300

    Dashboard panel container for all routes

--- src/components/Pages/Dashboard/dashboard-panel.js
+++ src/components/Pages/Dashboard/dashboard-panel.js
@@ -1,5 +1,11 @@
 function DashboardPanel() {
-  return <div>This is dashboard panel</div>
+  return (
+    <div className='is-flex is-full-width is-justify-content-space-between'>
+      <div className='card dashboard-card p-3'>First Card</div>
+      <div className='card p-3'>Second Card</div>
+      <div className='card p-3'>Third Card</div>
+    </div>
+  )
 }
 
 export default DashboardPanel
--- src/components/Pages/Dashboard/index.js
+++ src/components/Pages/Dashboard/index.js
@@ -6,9 +6,9 @@ function Dashboard() {
   return (
     <div className='dashboard-main'>
       <DashboardHeader />
-      <div className='columns'>
+      <div className='columns pl-2 pr-6 pt-3'>
         <DashboardMenu className='column is-one-fifth' />
-        <DashboardContent className='column is-four-fifths is-justify-content-center is-flex dashboard-content' />
+        <DashboardContent className='column is-four-fifths is-justify-content-space-between dashboard-card-container p-6 is-flex' />
       </div>
     </div>
   )
